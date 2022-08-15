5ea37080c9eecdf072a7a133ce148f4b2284372c
Josh <31908788+truemiller@users.noreply.github.com>
2022-08-15 20:36:55 +0100

    push for merge

---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function About(): JSX.Element {
+  return (
+    <div className={"p-10"}>
+      <div className="container mx-auto">About</div>
+    </div>
+  );
+}
